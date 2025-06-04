3ada6b16cd347374cb71810c1791a3ac017a3853
Allan Chiang <allanchiang24@gmail.com>
2025-06-04 16:58:30 +1200

    Close all dropdowns except for the current dropdown and add .active to dropdown class tag in styles.css

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ document.addEventListener("click", e => {
         currentDropdown = e.target.closest('[data-dropdown]')
         currentDropdown.classList.toggle('active')
     }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
 })
